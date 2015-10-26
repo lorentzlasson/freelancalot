eaaf2b49364c1e7fac96856128c35aa6b794cbde
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-10-26 12:37:28 +0100

    add custom user registry

--- models/user.js
+++ models/user.js
@@ -1,21 +1,31 @@
 //Getting the orm instance
 var orm = require("../model"),
     Seq = orm.Seq();
+var bcrypt = require('bcrypt-nodejs');
 
 //Creating our module
 module.exports = {
     model: {
-        name: {
-            type: Seq.STRING,
-            allowNull: false
-        },
+        name: Seq.STRING,
         email: Seq.STRING,
+        password: Seq.STRING,
         googleId: Seq.STRING,
         photo: Seq.STRING
     },
 
     options: {
-        freezeTableName: true
+        freezeTableName: true,
+        setterMethods: {
+            password: function(value) {
+                var hash = bcrypt.hashSync(value)
+                this.setDataValue('password',hash);
+            }
+        },
+        instanceMethods: {
+            validPassword: function(password) {
+                return bcrypt.compareSync(password, this.password);
+            }
+        }
     }
 
 }
