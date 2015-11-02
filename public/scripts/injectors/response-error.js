0ed1e91345eb302194d2c42f02715a6b7517f76e
Lorentz Lasson <lasson.lorentz@gmail.com>
2015-11-02 22:51:55 +0100

    fix error remains in injector

--- public/scripts/injectors/response-error.js
+++ public/scripts/injectors/response-error.js
@@ -1,11 +1,11 @@
-app.factory('RespErrInjector', ($injector) => {
+app.factory('RespErrInjector', ($injector, $q) => {
     var injector = {
         responseError: function(response) {
             if (response.status == 403) {
                 var state = $injector.get('$state');
                 state.go('out');
             }
-            return response;
+            return $q.reject(response);
         }
     };
     return injector;
