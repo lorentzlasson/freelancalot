b74e507e0cf9d827976697dd0363129928c9debc
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-11-16 19:34:38 +0100

    remove google login

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 var util = require('util');
 var session = require('express-session');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var LocalStrategy = require('passport-local').Strategy;
 var validator = require('validator');
 
@@ -29,42 +28,6 @@ module.exports = (app, url, appEnv, User) => {
             done(null, user);
         })
     });
-
-    // ----- Google -----
-    var googleOAuth = appEnv.getService('googleOAuth'),
-        googleOAuthCreds = googleOAuth.credentials;
-
-    passport.use(new GoogleStrategy({
-            clientID: googleOAuthCreds.clientID,
-            clientSecret: googleOAuthCreds.clientSecret,
-            callbackURL: util.format("http://%s%s", url, googleOAuthCreds.callbackPath)
-        },
-        (token, refreshToken, profile, done) => {
-            process.nextTick(() => {
-                User.findOrCreate({
-                        where: {
-                            googleId: profile.id
-                        },
-                        defaults: {
-                            name: profile.displayName,
-                            email: profile.emails[0].value,
-                            photo: profile.photos[0].value
-                        }
-                    })
-                    .spread((user, created) => {
-                        done(null, user);
-                    })
-            });
-        }));
-
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
-
-    app.get('/auth/google/callback',
-        passport.authenticate('google', {
-            successRedirect: '/'
-        }));
 
     // ----- Local -----
     var getUser = (req, res) => {
