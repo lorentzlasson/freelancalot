5b08672149856e6c41b6d355e7edb14c0b57c3c9
Lorentz Lasson <lasson.lorentz@gmail.com>
2016-01-18 21:13:58 +0100

    adhere to eslint

--- models/user.js
+++ models/user.js
@@ -1,35 +1,35 @@
 //Getting the orm instance
-var orm = require("../model"),
-    Seq = orm.Seq();
-var bcrypt = require('bcrypt-nodejs');
+var orm = require('../model'),
+	Seq = orm.Seq()
+var bcrypt = require('bcrypt-nodejs')
 
 //Creating our module
 module.exports = {
-    model: {
-        name: Seq.STRING,
-        email: Seq.STRING,
-        password: Seq.STRING,
-        googleId: Seq.STRING,
-        photo: Seq.STRING,
-        permission: {
-            type: Seq.STRING,
-            defaultValue: 'admin'
-        }
-    },
+	model: {
+		name: Seq.STRING,
+		email: Seq.STRING,
+		password: Seq.STRING,
+		googleId: Seq.STRING,
+		photo: Seq.STRING,
+		permission: {
+			type: Seq.STRING,
+			defaultValue: 'admin'
+		}
+	},
 
-    options: {
-        freezeTableName: true,
-        setterMethods: {
-            password: function(value) {
-                var hash = bcrypt.hashSync(value)
-                this.setDataValue('password',hash);
-            }
-        },
-        instanceMethods: {
-            validPassword: function(password) {
-                return bcrypt.compareSync(password, this.password);
-            }
-        }
-    }
+	options: {
+		freezeTableName: true,
+		setterMethods: {
+			password: function(value) {
+				var hash = bcrypt.hashSync(value)
+				this.setDataValue('password',hash)
+			}
+		},
+		instanceMethods: {
+			validPassword: function(password) {
+				return bcrypt.compareSync(password, this.password)
+			}
+		}
+	}
 
 }
