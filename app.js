b895f143a97ff499ae5a749216b09e73f6f52fb1
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2016-01-18 21:13:58 +0100

    adhere to eslint

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 var express = require('express'),
-    app = express();
-var util = require('util');
-var orm = require('./model');
+	app = express()
+var util = require('util')
+var orm = require('./model')
 var cfenv = require('cfenv'),
-    appEnv = cfenv.getAppEnv();
+	appEnv = cfenv.getAppEnv()
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-var isLocal = !process.env.VCAP_APPLICATION;
-var host = (process.env.VCAP_APP_HOST || "localhost");
-var port = (process.env.VCAP_APP_PORT || 3000);
-var url = isLocal ? util.format("%s:%d", host, port) : appEnv.app.uris[0];
+var isLocal = !process.env.VCAP_APPLICATION
+var host = (process.env.VCAP_APP_HOST || 'localhost')
+var port = (process.env.VCAP_APP_PORT || 3000)
+var url = isLocal ? util.format('%s:%d', host, port) : appEnv.app.uris[0]
 
 var dbService = appEnv.getService('myClearDB'),
-    dbCreds = dbService.credentials;
+	dbCreds = dbService.credentials
 
-orm.setup("./models", dbCreds.name, dbCreds.username, dbCreds.password, {
-    host: dbCreds.hostname,
-    dialect: 'mysql',
-    port: dbCreds.port,
-    pool: {
-        max: 2
-    }
-});
+orm.setup('./models', dbCreds.name, dbCreds.username, dbCreds.password, {
+	host: dbCreds.hostname,
+	dialect: 'mysql',
+	port: dbCreds.port,
+	pool: {
+		max: 2
+	}
+})
 
-var User = orm.model("user");
+var User = orm.model('user')
 
 var ensureAuthenticated = require('./auth')(app, url, appEnv, User)
 
-app.use('/', express.static(__dirname + '/public'));
-var v1router = require('./api/v1');
-app.use('/api/v1/', ensureAuthenticated, v1router);
+app.use('/', express.static(__dirname + '/public'))
+var v1router = require('./api/v1')
+app.use('/api/v1/', ensureAuthenticated, v1router)
 
-console.log('Running on %s:%d', host, port);
-app.listen(port, host);
+console.log('Running on %s:%d', host, port)
+app.listen(port, host)
--- auth.js
+++ auth.js
@@ -1,78 +1,78 @@
-var util = require('util');
-var jwt = require('jsonwebtoken');
-var validator = require('validator');
+var jwt = require('jsonwebtoken')
+var validator = require('validator')
 
 module.exports = (app, url, appEnv, User) => {
 
-    // ----- Local -----
-    app.post('/register', (req, res, next) => {
-        var credentials = req.body;
-        if (!validator.isEmail(credentials.username)) {
-            return res.status(400).send('username is not a valid email address');
-        }
-        User.findOrCreate({
-                where: {
-                    email: credentials.username
-                },
-                defaults: {
-                    password: credentials.password
-                }
-            })
-            .spread((user, created) => {
-                if (!created) {
-                    return res.status(400).send('username taken');
-                };
+	// ----- Local -----
+	app.post('/register', (req, res) => {
+		var credentials = req.body
+		if (!validator.isEmail(credentials.username)) {
+			return res.status(400).send('username is not a valid email address')
+		}
+		User.findOrCreate({
+			where: {
+				email: credentials.username
+			},
+			defaults: {
+				password: credentials.password
+			}
+		})
+		.spread((user, created) => {
+			if (!created) {
+				return res.status(400).send('username taken')
+			}
 
-                return res.end();
-            })
-    });
+			return res.end()
+		})
+	})
 
-    app.post('/login', (req, res, next) => {
-        var credentials = req.body,
-            username = credentials.username,
-            password = credentials.password;
+	app.post('/login', (req, res) => {
+		var credentials = req.body,
+			username = credentials.username,
+			password = credentials.password
 
-        User.findOne({
-            where: {
-                email: username
-            }
-        }).then((user) => {
-            if (!user) {
-                return res.status(401).send('username does not exist');
-            }
+		User.findOne({
+			where: {
+				email: username
+			}
+		}).then((user) => {
+			if (!user) {
+				return res.status(401).send('username does not exist')
+			}
 
-            if (!user.validPassword(password)) {
-                return res.status(401).send('incorrect password');
-            }
+			if (!user.validPassword(password)) {
+				return res.status(401).send('incorrect password')
+			}
 
-            var token = jwt.sign(user.email, process.env.JWT_SECRET, {
-                expiresInMinutes: 1440
-            });
+			var token = jwt.sign(user.email, process.env.JWT_SECRET, {
+				expiresInMinutes: 1440
+			})
 
-            return res.send({
-                token: token
-            });
+			return res.send({
+				token: token
+			})
 
-        }, (error) => {
-            return res.sendStatus(500);
-        })
-    });
+		}, (err) => {
+			console.err(err)
+			return res.sendStatus(500)
+		})
+	})
 
-    return (req, res, next) => {
-        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	return (req, res, next) => {
+		var token = req.body.token || req.query.token || req.headers['x-access-token']
 
-        if (token) {
-            jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-                if (err) {
-                    return res.status(403).send('failed to authenticate token');
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-            });
+		if (token) {
+			jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+				if (err) {
+					return res.status(403).send('failed to authenticate token')
+				} else {
+					req.decoded = decoded
+					next()
+				}
+			})
 
-        } else {
-            return res.status(403).send('no token provided');
-        }
-    }
+		} else {
+			return res.status(403).send('no token provided')
+		}
+	}
 }
