e03cade66a1b634346d72f38c859745a247ae376
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2016-01-27 17:15:46 +0100

    put db setup in root index.js

--- app/index.js
+++ app/index.js
@@ -1,24 +1,9 @@
 var express = require('express'),
 	app = express(),
-	orm = require('./orm'),
-	cfenv = require('cfenv'),
-	appEnv = cfenv.getAppEnv(),
 	auth = require('./auth'),
 	bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-
-var dbService = appEnv.getService('myClearDB'),
-	dbCreds = dbService.credentials
-
-orm.setup(dbCreds.name, dbCreds.username, dbCreds.password, {
-	host: dbCreds.hostname,
-	dialect: 'mysql',
-	port: dbCreds.port,
-	pool: {
-		max: 2
-	}
-})
 
 app.use(auth.router)
 app.use('/', express.static(__dirname + '/public'))
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var cfenv = require('cfenv'),
 	appEnv = cfenv.getAppEnv(),
-	app = require('./app')
+	app = require('./app'),
+	orm = require('./app/orm')
+
+var dbService = appEnv.getService('myClearDB'),
+	dbCreds = dbService.credentials
+
+orm.setup(dbCreds.name, dbCreds.username, dbCreds.password, {
+	host: dbCreds.hostname,
+	dialect: 'mysql',
+	port: dbCreds.port,
+	pool: {
+		max: 2
+	}
+})
 
 app.listen(appEnv.port)
 console.log('Running on %s', appEnv.url)
