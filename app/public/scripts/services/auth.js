a347dd4120422716ea2a1cd2e8a0480bfd947296
Robert Barlin <robert.barlin@gmail.com>
2016-03-29 22:41:48 +0200

    persist token in localstorage

--- app/public/scripts/services/auth.js
+++ app/public/scripts/services/auth.js
@@ -1,14 +1,24 @@
-app.factory('Auth', ($http) => {
+app.factory('Auth', ($http, $window) => {
 	this.token
+
+	function loadToken(){
+		this.token = $window.localStorage['token']
+		return this.token
+	}
+
+	function saveToken(token){
+		this.token = token
+		$window.localStorage['token'] = token
+	}
 	
 	return {
 		getToken: () => {
-			if(!this.token) throw new Error('no token')
+			if(!loadToken()) throw new Error('no token')
 			return this.token
 		},
 
 		setToken: token => {
-			this.token = token
+			saveToken(token)
 		}
 	}
 })
